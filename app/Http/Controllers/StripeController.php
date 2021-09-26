6f2adf7d9703ec1fa386c8bff09faf143e8bcbbc
sean cartman <cartman.david@gmail.com>
2021-09-26 16:53:25 +1300

    stripe wip

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -3,28 +3,87 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Stripe;
 use Session;
+use Stripe\Charge;
+use Stripe\Stripe;
 
 class StripeController extends Controller
 {
     public function create()
     {
-        return view('stripe.create');
+        return view('payment');
     }
 
     public function store(Request $request)
     {
-        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-        Stripe\Charge::create ([
-                "amount" => 100 * 100,
-                "currency" => "usd",
-                "source" => $request->stripeToken,
-                "description" => "This payment is tested purpose myfamilycookbook"
+        request()->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'terms_conditions' => 'accepted'
         ]);
-   
-        Session::flash('success', 'Payment successful!');
-           
-        return back();
+
+        /** I have hard coded amount. You may fetch the amount based on customers order or anything */
+        $amount     = 1 * 100;
+        $currency   = 'usd';
+
+        if (empty(request()->get('stripeToken'))) {
+            session()->flash('error', 'Some error while making the payment. Please try again');
+            return back()->withInput();
+        }
+        Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+        try {
+            /** Add customer to stripe, Stripe customer */
+            $customer = Customer::create([
+                'email'     => request('email'),
+                'source'    => request('stripeToken')
+            ]);
+        } catch (Exception $e) {
+            $apiError = $e->getMessage();
+        }
+
+        if (empty($apiError) && $customer) {
+            /** Charge a credit or a debit card */
+            try {
+                /** Stripe charge class */
+                $charge = Charge::create(array(
+                    'customer'      => $customer->id,
+                    'amount'        => $amount,
+                    'currency'      => $currency,
+                    'description'   => 'Some testing description'
+                ));
+            } catch (Exception $e) {
+                $apiError = $e->getMessage();
+            }
+
+            if (empty($apiError) && $charge) {
+                // Retrieve charge details 
+                $paymentDetails = $charge->jsonSerialize();
+                if ($paymentDetails['amount_refunded'] == 0 && empty($paymentDetails['failure_code']) && $paymentDetails['paid'] == 1 && $paymentDetails['captured'] == 1) {
+                    /** You need to create model and other implementations */
+                    /*
+                    Payment::create([
+                        'name'                          => request('name'),
+                        'email'                         => request('email'),
+                        'amount'                        => $paymentDetails['amount'] / 100,
+                        'currency'                      => $paymentDetails['currency'],
+                        'transaction_id'                => $paymentDetails['balance_transaction'],
+                        'payment_status'                => $paymentDetails['status'],
+                        'receipt_url'                   => $paymentDetails['receipt_url'],
+                        'transaction_complete_details'  => json_encode($paymentDetails)
+                    ]);
+                    */
+                    return redirect('/thankyou/?receipt_url=' . $paymentDetails['receipt_url']);
+                } else {
+                    session()->flash('error', 'Transaction failed');
+                    return back()->withInput();
+                }
+            } else {
+                session()->flash('error', 'Error in capturing amount: ' . $apiError);
+                return back()->withInput();
+            }
+        } else {
+            session()->flash('error', 'Invalid card details: ' . $apiError);
+            return back()->withInput();
+        }
     }
 }
--- resources/views/mail/contact.blade.php
+++ resources/views/mail/contact.blade.php
@@ -29,14 +29,12 @@
         @csrf
         <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
             <label for="email">Email address</label>
-            <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp"
-                placeholder="Enter your email">
+            <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter your email">
             <span class="text-danger">{{ $errors->first('email') }}</span>
         </div>
         <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
             <label for="name">Name</label>
-            <input name="name" type="text" class="form-control" id="name" aria-describedby="name"
-                placeholder="Your name">
+            <input name="name" type="text" class="form-control" id="name" aria-describedby="name" placeholder="Your name">
             <span class="text-danger">{{ $errors->first('name') }}</span>
 
         </div>
@@ -62,46 +60,74 @@
                 <p>{{ Session::get('success') }}</p>
             </div>
             @endif
-            <form role="form" action="{{ route('payments.store') }}" method="post" class="require-validation"
-                data-cc-on-file="false" data-stripe-publishable-key="{{ env('STRIPE_KEY') }}" id="payment-form">
+            <form role="form" action="{{ route('payments.store') }}" method="post" id="payment-form">
                 @csrf
-                <div class='row'>
-                    <div class='col-xs-12 form-group required'>
-                        <label class='control-label'>Name on Card</label> <input class='form-control' size='4'
-                            type='text'>
+                <div class="form-row">
+                    <label for="card-element">
+                        Credit or debit card
+                    </label>
+                    <div id="card-element">
+                        <!-- A Stripe Element will be inserted here. -->
                     </div>
+
+                    <!-- Used to display Element errors. -->
+                    <div id="card-errors" role="alert"></div>
                 </div>
-                <div class='row'>
-                    <div class='col-xs-12 form-group required'>
-                        <label class='control-label'>Card Number</label> <input autocomplete='off'
-                            class='form-control card-number' size='20' type='text'>
-                    </div>
-                </div>
-                <div class='row'>
-                    <div class='col-xs-12 col-md-4 form-group cvc required'>
-                        <label class='control-label'>CVC</label> <input autocomplete='off' class='form-control card-cvc'
-                            placeholder='ex. 311' size='4' type='text'>
-                    </div>
-                    <div class='col-xs-12 col-md-4 form-group expiration required'>
-                        <label class='control-label'>Expiration Month</label> <input
-                            class='form-control card-expiry-month' placeholder='MM' size='2' type='text'>
-                    </div>
-                    <div class='col-xs-12 col-md-4 form-group expiration required'>
-                        <label class='control-label'>Expiration Year</label> <input
-                            class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text'>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-12 mt-2">
-                        <button class="btn btn-primary btn btn-block" type="submit">$100 USD</button>
-                    </div>
-                </div>
+
+                <button>Submit Payment</button>F
             </form>
         </div>
     </div>
 </div>
-</div>
-</div>
+<script src="https://js.stripe.com/v3/"></script>
+<script>
+    // Custom styling can be passed to options when creating an Element.
+    var style = {
+        base: {
+            // Add your base input styles here. For example:
+            fontSize: '16px',
+            color: '#32325d',
+        },
+    };
+
+    // Create an instance of the card Element.
+    var card = elements.create('card', {style: style });
+
+    // Add an instance of the card Element into the `card-element` <div>.
+    card.mount('#card-element');
+    var stripe = Stripe("{{ env('STRIPE_KEY') }}");
+    var elements = stripe.elements();
+
+    // Create a token or display an error when the form is submitted.
+    var form = document.getElementById('payment-form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        stripe.createToken(card).then(function(result) {
+            if (result.error) {
+                // Inform the customer that there was an error.
+                var errorElement = document.getElementById('card-errors');
+                errorElement.textContent = result.error.message;
+            } else {
+                // Send the token to your server.
+                stripeTokenHandler(result.token);
+            }
+        });
+    });
+
+    function stripeTokenHandler(token) {
+        // Insert the token ID into the form so it gets submitted to the server
+        var form = document.getElementById('payment-form');
+        var hiddenInput = document.createElement('input');
+        hiddenInput.setAttribute('type', 'hidden');
+        hiddenInput.setAttribute('name', 'stripeToken');
+        hiddenInput.setAttribute('value', token.id);
+        form.appendChild(hiddenInput);
+
+        // Submit the form
+        form.submit();
+    }
+</script>
 
 
 @endsection
